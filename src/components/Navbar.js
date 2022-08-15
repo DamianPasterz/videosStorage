60433035bd85be3503f0605773fe4f30661013a4
Damian Pasterz <damian.pasterz@gmail.com>
2022-08-15 18:22:02 +0200

    code refactoring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useVideoContext } from '../context/Video_context';
+import { useVideoContext } from '../context/VideoContext';
 import "../index.css"
 
 import { FaHeart } from 'react-icons/fa';
@@ -7,7 +7,7 @@ import { FaHeart } from 'react-icons/fa';
 const NavbarComponent = () => {
     const { setStatus } = useVideoContext();
 
-    const HandleAll = () => {
+    const handleAll = () => {
         setStatus("all")
     }
     const handleStatus = (e) => {
@@ -16,10 +16,9 @@ const NavbarComponent = () => {
     return (
 
         <div className='nawbar__body'>
-            <div className='nawbar__btn' onClick={HandleAll}>
-                <p> All VIDEOS</p>
+            <div className='nawbar__btn' onClick={handleAll}>
+                All VIDEOS
             </div>
-
             <div className='nawbar__btn' onClick={handleStatus} >
                 FAVOURITE <FaHeart size='1.5rem' color='red' id='heart' />
             </div>
